14c338ed9d1e385f9a5697a20cdae6ef304b7aee
AugmentVista <AuAgments@gmail.com>
2025-01-26 13:24:25 -0400

    Works for first bounce
    Something is off with the bubbles that are created

--- bubble-drop-godot-4/BubblePlatform(Wip)/bubble_explosion.gd
+++ bubble-drop-godot-4/BubblePlatform(Wip)/bubble_explosion.gd
@@ -1,17 +1,17 @@
-extends AnimatableBody2D
-@onready var RightSideBubble = $Area2DRightSideBubble
-@onready var LeftSideBubble = $Area2DLeftSideBubble
-# Called when the node enters the scene tree for the first time.
+class_name BubbleExplosion extends Node2D
 
 var thrust = Vector2(0, -250)
+var angle = rotation
+var impulse_force_strength = 400
+var rng = RandomNumberGenerator.new()
 
-func _on_Area2_area_entered(area):
-	if area.is_in_group("player"):
-		print("collided with player")
 
-#func _integrate_forces(state):
-	#Replace with collision detection with player
-	#if Input.is_action_pressed("ui_up"):
-		#set direction to a range -30 to 0 for left, 0 to 30 for right.
-		#var rotation_direction = 0
-		#state.apply_force(thrust.rotated(rotation))
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Collision")
+	var random_rotation = rng.randf_range(-10.0, 10.0)
+	var collision_info = (impulse_force_strength * thrust )
+	if body is CharacterBody2D:
+		var collision_impulse = thrust.rotated(rotation * random_rotation).normalized() * impulse_force_strength
+		body.velocity += collision_impulse
+		
+		print("Collided with player")
