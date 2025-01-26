1cb45a9da4160c0368cb4fb6b63df151fd99180e
AugmentVista <AuAgments@gmail.com>
2025-01-26 10:54:35 -0400

    bubble explosions' script (unfinished)

---
+++ bubble-drop-godot-4/BubblePlatform(Wip)/bubble_explosion.gd
@@ -0,0 +1,13 @@
+extends AnimatableBody2D
+@onready var RightSideBubble = $Area2DRightSideBubble
+@onready var LeftSideBubble = $Area2DLeftSideBubble
+# Called when the node enters the scene tree for the first time.
+
+var thrust = Vector2(0, -250)
+
+func _integrate_forces(state):
+	#Replace with collision detection with player
+	if Input.is_action_pressed("ui_up"):
+		#set direction to a range -30 to 0 for left, 0 to 30 for right.
+		var rotation_direction = 0
+		state.apply_force(thrust.rotated(rotation))
