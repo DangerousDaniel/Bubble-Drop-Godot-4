d12809dbefb026592d594df111be8bcaa99b4e2b
AugmentVista <AuAgments@gmail.com>
2025-01-26 09:36:11 -0400

    Code comments

--- bubble-drop-godot-4/BubblePlatform(Wip)/spawnBubble.gd
+++ bubble-drop-godot-4/BubblePlatform(Wip)/spawnBubble.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+#File path to the Bubble platform object
 var mynode = preload ("res://BubblePlatform(Wip)/bubble_platform.tscn")
 
+# During each update of the game, this checks for if mouse1 is pressed; creates a bubble in that space
 func _physics_process(delta):
 	if Input.is_action_just_pressed("CreateBubble"):
 		inst(get_global_mouse_position())
 
-
+# method/function to instantiate bubble object
 func inst(pos):
 	var instance = mynode.instantiate()
 	instance.position = pos
