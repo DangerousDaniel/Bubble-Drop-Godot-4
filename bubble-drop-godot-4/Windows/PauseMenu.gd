91812e710767c7ee89086ffae5defe551a7d4b20
DangerousDaniel <danielcox996@gmail.com>
2025-01-29 10:18:48 -0400

    disable the player inputs for a few seconds after leave a pop up screen

--- bubble-drop-godot-4/Windows/PauseMenu.gd
+++ bubble-drop-godot-4/Windows/PauseMenu.gd
@@ -19,21 +19,35 @@ var IsGamePaused : bool = false:
 
 #Game Events Methods
 func ToggleGamePaused():
-	IsGamePaused = !IsGamePaused
-	get_tree().paused = IsGamePaused	
+	if IsGamePaused == false:
+		IsGamePaused = true
+		get_tree().paused = true
+	elif IsGamePaused ==  true:
+		IsGamePaused = false
+		get_tree().paused = false	
 
 #Godot Methods
 func _process(delta: float) -> void:
 	#Pause Inputs
 	if (Input.is_action_just_pressed("ui_cancel")):
 		if not $Panel.is_visible_in_tree():
+			ToggleGamePaused()
 			$Panel.show()
-		else: 
+		else:
+			ToggleGamePaused()
+			set_process_input(false) 
+			await get_tree().create_timer(3).timeout
+			set_process_input(true) 
 			$Panel.hide()
+			
 
 #Signal Methods			
 func _on_back_to_main_menu_button_button_down() -> void:
+	ToggleGamePaused()
 	get_tree().change_scene_to_file("res://Levels/MainMenu/MainMenuLevel.tscn")
 
 func _on_back_to_game_button_button_down() -> void:
+	set_process_input(false) 
+	await get_tree().create_timer(3).timeout
+	set_process_input(true) 
 	$Panel.hide()
